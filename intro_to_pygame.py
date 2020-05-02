b76c3819efea76841261174a422616f8fa0cb1f9
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-02 07:56:29 +0300

    Create a game display window

--- intro_to_pygame.py
+++ intro_to_pygame.py
@@ -2,11 +2,28 @@
 #intro_to_pygame.py - shows how to use the pygame package for the very first time
 
 # TODO 1: Import pygame
+import pygame
 
 # TODO 2: Initialize pygame
+pygame.init()
 
 # TODO 3: Instantiate the game display
-
-# TODO 4: Learn how to close the game display using the keyboard
+display_width = 500
+display_height = 500
+game_display = pygame.display.set_mode((display_width, display_height))
 
 # TODO 5: Update the game window caption
+pygame.display.set_caption('Intro to Pygame')
+
+pygame.display.update()
+
+# TODO 4: Learn how to close the game display using the keyboard
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and (
+                event.key == pygame.K_ESCAPE or 
+                event.key == pygame.K_q                
+            )):
+            pygame.quit()
+            quit()
