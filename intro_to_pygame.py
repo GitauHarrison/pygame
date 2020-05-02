41f3bc5403f5590d1c9d922ae64bfb7c429d7b92
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-02 07:31:45 +0300

    Create into intro_to_pygame.py file and explain how to set up pygame

---
+++ intro_to_pygame.py
@@ -0,0 +1,12 @@
+# !python3
+#intro_to_pygame.py - shows how to use the pygame package for the very first time
+
+# TODO 1: Import pygame
+
+# TODO 2: Initialize pygame
+
+# TODO 3: Instantiate the game display
+
+# TODO 4: Learn how to close the game display using the keyboard
+
+# TODO 5: Update the game window caption
