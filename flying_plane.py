b28fdffc7ba1b4d3aa2b6dd8dcec93996c05fb71
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-22 17:06:31 +0300

    Add a player character

--- flying_plane.py
+++ flying_plane.py
@@ -6,6 +6,15 @@ screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flying Enemy Territory')
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+player = Player()
+
 def event_handler():
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (
@@ -16,16 +25,13 @@ def event_handler():
             quit()
 
 while True:
-    screen.fill((255, 255, 255))
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-    
+    screen.fill((0, 0, 0))
+
     surf_center =(
-        (screen_width - surf.get_width())/2,
-        (screen_height - surf.get_width())/2
+        (screen_width - player.surf.get_width())/2,
+        (screen_height - player.surf.get_width())/2
     )
-    screen.blit(surf, surf_center)
+    screen.blit(player.surf, player.rect)
 
     event_handler()
     pygame.display.update()
