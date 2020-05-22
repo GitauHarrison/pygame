72864eb4fedf5e76aca0795dc908dc8a8820a7ec
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-22 07:39:41 +0300

    Fill the screen with color and bit a rectangle

--- flying_plane.py
+++ flying_plane.py
@@ -1,7 +1,9 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flying Enemy Territory')
 
 def event_handler():
@@ -12,6 +14,14 @@ def event_handler():
             )):
             pygame.quit()
             quit()
+
 while True:
+    screen.fill((255, 255, 255))
+    surf = pygame.Surface((50, 50))
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
+    screen.blit(surf, (screen_width/2, screen_height/2))
+
+
     event_handler()
     pygame.display.update()
