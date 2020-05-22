920671065eee0a61bd9e9e825cb358b65b09144b
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-22 17:08:49 +0300

    Add a player character

--- flying_plane.py
+++ flying_plane.py
@@ -1,4 +1,13 @@
 import pygame
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
 
 pygame.init()
 screen_width = 500
@@ -34,4 +43,16 @@ while True:
     screen.blit(player.surf, player.rect)
 
     event_handler()
-    pygame.display.update()
+    player.update()
+
+pressed_keys = pygame.key.get_pressed()
+def update(self, pressed_keys):
+    if pressed_keys[K_UP]:
+        self.rect.move_ip(0, -5)
+    if pressed_keys[K_DOWN]:
+        self.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        self.rect.move_ip(-5, 0)
+    if pressed_keys[K_RIGHT]:
+        self.rect.move_ip(5, 0)
+
