fbaad4ef59ffe8df0ce79f7a80aef1e758b011dd
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-22 06:18:10 +0300

    Add event handling

--- flying_plane.py
+++ flying_plane.py
@@ -4,3 +4,14 @@ pygame.init()
 screen = pygame.display.set_mode([500, 500])
 pygame.display.set_caption('Flying Enemy Territory')
 
+def event_handler():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and (
+                event.type == pygame.K_ESCAPE or pygame.K_q
+            )):
+            pygame.quit()
+            quit()
+while True:
+    event_handler()
+    pygame.display.update()
