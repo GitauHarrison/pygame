195dcf7b9480502a72390372dfb9d2d5c7aa3046
Gitau Harrison <harrisongitau3@gmail.com>
2020-05-31 04:51:26 +0300

    Add colors which define the play area

--- simple_games/memory_puzzle_game/memroy_puzzle.py
+++ simple_games/memory_puzzle_game/memroy_puzzle.py
@@ -12,3 +12,21 @@ board_height = 7
 assert (board_width * board_height) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 x_margin = int((window_width - (board_width * (box_size + gap_size))) / 2)
 y_margin = int((window_height - (board_height * (box_size + gap_size))) / 2)
+
+gray = (100, 100, 100)
+navy_blue = (60, 60, 100)
+white = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+orange = (255, 128, 0)
+purple = (255, 0, 255)
+cyan = (0, 255, 255)
+
+bg_color = navy_blue
+light_bg_color = gray
+box_color = white
+highlight_color = blue
+
+donut = 'donut'
